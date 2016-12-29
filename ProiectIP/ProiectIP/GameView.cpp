3e8087cb2d4153c644ac3f126a10679c056bbaeb
Florin Velesca <velesca.florin97@gmail.com>
2016-12-29 22:40:34 +0200

    cleaned up gameview

--- ProiectIP/ProiectIP/GameView.cpp
+++ ProiectIP/ProiectIP/GameView.cpp
@@ -4,20 +4,6 @@ GameView::GameView(SDL_Window *window, SDL_Surface *surface)
 {
 	this->window = window;
 	this->screen = surface;
-	Uint32 frontColor = SDL_MapRGB(screen->format, 255, 255, 255);
-	Uint32 backColor = SDL_MapRGB(screen->format, 230, 74, 25);
-	Uint32 greenColor = SDL_MapRGB(screen->format, 205, 220, 57);
-	int width = surface->w/7;
-	int height = surface->h/4;
-
-	/*
-	Card card("A", frontColor, backColor, margin, margin, width, height);
-	cards.push_back(card);
-
-	Card card2("Q", frontColor, backColor, margin+width+10,margin, width, height, false);
-	cards.push_back(card2);*/
-
-	
 }
 void GameView::displayGameInProgressMode(GameController* gameController, int balance, int currentBet, char *dealerCard[], char *playerCard[]) {
 	clearShapes();
